9e188df0a6ca74c9a06d69c8716203070c7fc03f
Carlos Caralps Rueda <carlosca2001@gmail.com>
2021-03-11 21:57:57 +0100

    Proofs T1_characterisation & T3_is_T2

--- src/separacio.lean
+++ src/separacio.lean
@@ -73,7 +73,38 @@ end
 lemma T1_characterisation (X : Type) [topological_space X] :
   is_frechet_space X ↔ (∀ (x : X), is_closed ({x} : set X)) :=
 begin
-  sorry
+  split,
+  {
+    intros h x,
+    unfold is_closed,
+    let I := {U : set X | (x ∉ U) ∧ (is_open U)},
+    have p : ⋃₀ I = {x}ᶜ,
+    {
+      apply subset.antisymm; intros t ht, 
+      {
+        rcases ht with ⟨A,⟨hxA, hA⟩, htA⟩,
+        simp,
+        intro htx,
+        rw htx at htA,
+        exact hxA htA,     
+      },
+      {
+        have htx := (mem_compl_singleton_iff.mp ht).symm,
+        obtain ⟨U, hU, hh⟩ := h t x htx,
+        use U,
+        exact ⟨⟨hh.2, hU⟩, hh.1⟩,
+      }
+    },
+    rw ← p,
+    have c : ∀ B ∈ I, is_open B,
+      finish,
+    exact topological_space.union I c,
+  },
+  {
+    intros h x y hxy,
+    use {y}ᶜ,
+    exact ⟨h y, mem_compl_singleton_iff.mpr (ne.symm hxy), not_not.mpr rfl⟩,
+  }
 end
 
 end frechet_space
@@ -88,32 +119,32 @@ instance T2_is_T1 (X : Type) [hausdorff_space X] : frechet_space X :=
 { t1 := 
 begin
   intros x y hxy,
-  obtain ⟨U, V, hU, hV, hUV, hh⟩ := hausdorff_space.t2 x y hxy,
+  obtain ⟨U, V, hU, hV, hUV, hh⟩ := t2 x y hxy,
   rw inter_comm at hUV,
   use U,
-  split,
-    exact hU,
-    exact ⟨hh.1, (inter_is_not_is_empty_intersection hh.2 hUV)⟩,
+  exact ⟨hU, ⟨hh.1, (inter_is_not_is_empty_intersection hh.2 hUV)⟩⟩,
 end }
 
 end hausdorff_space
 
 def is_regular (X : Type) [topological_space X] :=
   ∀ (x : X) (F : set X) (hF : is_closed F) (hxF: x ∉ F),
-  ∃ (U V : set X) (hU : is_open U) (hV : is_open V), (x ∈ U) ∧ (F ⊆ V)
+  ∃ (U V : set X) (hU : is_open U) (hV : is_open V) (hUV : U ∩ V = ∅), (x ∈ U) ∧ (F ⊆ V)
 
 class T3_space (X : Type) extends frechet_space X :=
 (regular : is_regular X)
 
 namespace T3_space
+open frechet_space
 
-lemma T3_is_T2 [T3_space X] : hausdorff_space X :=
+instance T3_is_T2 (X : Type) [T3_space X] : hausdorff_space X :=
 { t2 := 
 begin
   intros x y hxy,
-  --obtain h := regular_hausdorff_space.regular.regular,
-  --obtain ⟨U, hU, hh⟩ := regular_hausdorff_space.frechet.t1 x y hxy,
-  sorry
+  obtain ⟨U, V, hU, hV, hUV, hh⟩  := regular y {x} ((T1_characterisation X).1 t1 x) hxy,
+  rw inter_comm U V at hUV,
+  use V, use U,
+  exact ⟨hV, ⟨hU, ⟨hUV, ⟨singleton_subset_iff.mp hh.2, hh.1⟩⟩⟩⟩, 
 end}
 
 end T3_space
