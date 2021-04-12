7cac4d68ccacd5e9a5e9763198215bfc8ef55b9c
Eloi Torrents <eloitorrentsj@gmail.com>
2021-04-12 23:39:44 +0200

    Level 2 - basic_defs_world

---
+++ src/game/basic_defs_world/level2.lean
@@ -0,0 +1,29 @@
+import .level1 -- hide
+open set --hide
+namespace topological_space -- hide
+
+
+/-
+ # Level 2: The union of two open sets is open
+-/
+
+/- Lemma
+Prove that the union of two open sets is open.
+-/
+lemma open_of_union {X : Type} [topological_space X] {U V : set X}
+(hU : is_open U) (hV : is_open V): is_open (U ∪ V) :=
+begin
+  let I : set (set X) := {U, V},
+  have H : ⋃₀ I = U ∪ V := sUnion_pair U V,
+  rw ←H,
+  apply union I,
+  intros B hB,
+  replace hB : B = U ∨ B = V, by tauto,
+  cases hB; {rw hB, assumption},
+
+
+
+
+end
+
+end topological_space --hide
