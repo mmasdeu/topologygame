9631f7cf1609367887b15309bb0fc7a650344949
Marc <marc.masdeu@gmail.com>
2021-09-30 10:37:56 +0200

    Update level2.lean

--- src/basic_defs_world/level2.lean
+++ src/basic_defs_world/level2.lean
@@ -4,11 +4,11 @@ namespace topological_space --hide
 
 
 /-
- # Level 2: The union of two open sets is open
+# Level 2: Union of two open sets
 -/
 
 /- Lemma
-Prove that the union of two open sets is open.
+The union of two open sets is open.
 -/
 lemma open_of_union {X : Type} [topological_space X] {U V : set X}
 (hU : is_open U) (hV : is_open V): is_open (U ∪ V) :=
