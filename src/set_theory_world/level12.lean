2ba320cbfc24c88a941c872cb847f6f49321e20b
Marc <marc.masdeu@gmail.com>
2022-02-23 11:28:38 +0100

    Apply suggestions from code review
    Co-authored-by: Carlos <78864605+carloscaralps@users.noreply.github.com>

--- src/set_theory_world/level12.lean
+++ src/set_theory_world/level12.lean
@@ -3,9 +3,13 @@ import data.set.finite -- hide
 open set -- hide
 
 /- Tactic : use
+
+## Summary
+The tactic use specializes the goal with a particular case. For example, if we want to prove the statement "there exists a natural number which is odd", we will need to provide a concrete number like 3. 
+
 -/
 
-/- Lemma : If A and B are sets, then there exists a set S such that S ⊆ A ∩ B.
+/- Lemma : If A and B are subsets of a fixed set, then there exists a subset S such that S ⊆ A ∩ B.
 
 -/
 lemma example_on_use (α : Type) (A : set α) (B : set α) : ∃ S : set α, S ⊆ A ∩ B :=
@@ -13,13 +17,10 @@ begin
   use ∅,
   intros h h1,
   split,
-  repeat{
+  repeat
+  {
     exfalso,
-    exact h1,
-  },
-
-
-
-
+    exact h1
+  }
 
 end
